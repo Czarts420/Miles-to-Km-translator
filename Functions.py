b4bb002e27f0d1a2fa96b93f2bfab781c4338ca7
Czarts420 <71275157+Czarts420@users.noreply.github.com>
2021-02-03 10:07:38 +0000

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,11 +1,11 @@
-def functionOne(localvariable):
-  localvariable="hello"
-  print (localvariable)
+def functionOne(localvariable):# this is a function that stores a string of data 
+  localvariable="hello"#
+  print (localvariable)# this prints the variable 
 
-def functionTwo(parameter):
-  print (parameter)
+def functionTwo(parameter):# this is a function that stores the parameter 
+  print (parameter)# this prints the parameter
 
-def functionThree(globalNumericvalue):
+def functionThree(globalNumericvalue):# this is a function that stores a global variable 
   global globalNumericvalue
   globalNumericvalue = globalNumericvalue + 2
-  print(globalNumericvalue)
+  print(globalNumericvalue)# this prints the global variable 
