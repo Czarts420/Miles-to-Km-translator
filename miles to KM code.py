822790c2ea444ddcd3ebc9d4e0beed0ba704fa9f
Czarts420 <71275157+Czarts420@users.noreply.github.com>
2021-02-03 10:17:20 +0000

    Update miles to KM code.py

--- miles to KM code.py
+++ miles to KM code.py
@@ -1,8 +1,8 @@
 #Sequencing-----
-mile = int(input("Please Enter your miles: "))
-KM = mile * 1.6
-print("Your distance is: ", KM, "in KM")
-if mile >= 100:
+mile = int(input("Please Enter your miles: "))# this allowes the user to input an integer which will be stored  
+KM = mile * 1.6# this takes the stored integer and does a calculation 
+print("Your distance is: ", KM, "in KM")# this prints the new integer 
+if mile >= 100:# if the new integer is 100 or above it prints the line below
   print("I think you can stop now")
-else:
+else:# if the new integer is below 100 it prints the line below 
   print("You can drive further")
