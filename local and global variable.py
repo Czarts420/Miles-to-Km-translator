31bb42a9131335ab4fba81a47a87bf7f398364cf
Czarts420 <71275157+Czarts420@users.noreply.github.com>
2021-02-03 10:12:34 +0000

    Update local and global variable.py

--- local and global variable.py
+++ local and global variable.py
@@ -1,12 +1,12 @@
-def myfunc():
+def myfunc():# this is a function storing a global variable 
   global x
-  x = "a bad idea"
+  x = "a bad idea"# this is the global variable 
 
-myfunc()
+myfunc()# the global variable can be used out of the function 
 
-print("Second lockdown is " + x)
+print("Second lockdown is " + x)# this prints the global variable 
 
-def SayHello():
+def SayHello():# this is a function storing a local variable 
     user='John'
-    print ("user = ", SayHello)
-SayHello()
+    print ("user = ", SayHello)# this prints the local variable 
+SayHello()# the local variable can only be used within the function
