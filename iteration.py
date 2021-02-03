efba5995f7cfd60ec96710600ce6baa38f82db5a
Czarts420 <71275157+Czarts420@users.noreply.github.com>
2021-02-03 10:09:18 +0000

    Update iteration.py

--- iteration.py
+++ iteration.py
@@ -1,6 +1,6 @@
-mytuple = ("apple", "banana", "cherry")
+mytuple = ("apple", "banana", "cherry")# this is a list of strings 
 myit = iter(mytuple)
 
-print(next(myit))
-print(next(myit))
+print(next(myit))# this prints the first string in the list 
+print(next(myit))# this prints the next string 
 print(next(myit))
